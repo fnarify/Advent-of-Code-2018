5a5049e942fe348a8da5ecd4957f442670589b21
fnarify <16514203+fnarify@users.noreply.github.com>
2019-01-16 22:03:11 +1100

    changed to a struct

--- 23/aoc23.c
+++ 23/aoc23.c
@@ -5,29 +5,37 @@
 
 #define NPTS 1000
 
+struct Point {
+    int x, y, z, r;
+};
+typedef struct Point Point;
+
 int mdist(int x, int x2, int y, int y2, int z, int z2) {return abs(x - x2) + abs(y - y2) + abs(z - z2);}
 
 int main()
 {
-    int i, j, k, n, index, range = 0, inrange, div, high;
+    int i, j, k, n, index, range, inrange, div, high;
     char line[100];
     // bounds are fixed based on the approximate first 2 digits of the min and max values
     int bound[6] = {-20, 100, -20, 100, -20, 100}, pos[3] = {0};
-    int x[NPTS], y[NPTS], z[NPTS], r[NPTS];
-    int xt[NPTS], yt[NPTS], zt[NPTS], rt[NPTS];
+    Point p[NPTS], pdiv[NPTS];
 
-    i = 0;
+    i = index = range = 0;
     while (fgets(line, sizeof(line), stdin) != NULL)
     {
-        sscanf(line, "pos=<%d,%d,%d>, r=%d\n", &x[i], &y[i], &z[i], &r[i]);
-        if (r[i] > range) {range = r[i]; index = i;}
+        sscanf(line, "pos=<%d,%d,%d>, r=%d\n", &p[i].x, &p[i].y, &p[i].z, &p[i].r);
+        if (p[i].r > range)
+        {
+            range = p[i].r;
+            index = i;
+        }
         i++;
     }
 
     inrange = 0;
     for (i = 0; i < NPTS; i++)
     {
-        if (mdist(x[i], x[index], y[i], y[index], z[i], z[index]) <= range) {inrange++;}
+        if (mdist(p[i].x, p[index].x, p[i].y, p[index].y, p[i].z, p[index].z) <= range) {inrange++;}
     }
     printf("Part 1: %d\n", inrange);
 
@@ -38,11 +46,12 @@ int main()
         high = 0;
         for (i = 0; i < NPTS; i++)
         {
-            xt[i] = x[i] / div;
-            yt[i] = y[i] / div;
-            zt[i] = z[i] / div;
-            rt[i] = r[i] / div;
-            if (div > 1) {rt[i]++;} // edge case
+            pdiv[i].x = p[i].x / div;
+            pdiv[i].y = p[i].y / div;
+            pdiv[i].z = p[i].z / div;
+            pdiv[i].r = p[i].r / div;
+            // edge case
+            if (div > 1) {pdiv[i].r++;}
         }
 
         for (i = bound[0]; i < bound[1]; i++)
@@ -54,7 +63,7 @@ int main()
                     inrange = 0;
                     for (n = 0; n < NPTS; n++)
                     {
-                        if (mdist(i, xt[n], j, yt[n], k, zt[n]) <= rt[n]) {inrange++;}
+                        if (mdist(i, pdiv[n].x, j, pdiv[n].y, k, pdiv[n].z) <= pdiv[n].r) {inrange++;}
                     }
                     if (inrange > high)
                     {
@@ -68,16 +77,16 @@ int main()
         }
 
         // change bounds
-        printf("%d %d %d]\n", pos[0], pos[1], pos[2]);
+        printf("[%d %d %d]\n", pos[0], pos[1], pos[2]);
         for (i = 0; i < 6; i++)
         {
             bound[i] = pos[i / 2] * 10 + ((2 * (i % 2)) - 1) * 100;
-            printf("%d, ", bound[i]);
+            printf("%d%s", bound[i], i != 5 ? ", " : "");
         }
         printf("\n");
     }
 
-    printf("Part 2: %d dist in range of %d points\n", pos[0] + pos[1] + pos[2], high);
+    printf("Part 2: %d dist in range of %d points\n", abs(pos[0]) + abs(pos[1]) + abs(pos[2]), high);
 
     return 0;
 }
