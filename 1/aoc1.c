1deb1e04c6b96b089e37688a4a0d3d12a7f503df
fnarify <16514203+fnarify@users.noreply.github.com>
2019-01-30 22:37:43 +1100

    removed hardcoded values

--- 1/aoc1.c
+++ 1/aoc1.c
@@ -1,61 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
-#define MAX_LINE 10
-#define NUM_LOOP 140 // 140 for first duplicate
+#define MAX_LINE 20
+#define IN_LEN   1200
+#define TOLR     1000
 
 /**
- * Advent of Code 1:
- * Prints the first duplicate value from the summation list input1.txt
- * if NUM_LOOP is set to 1 it will print the sum after just 1 iteration.
+ * Prints the sum of the values after one iteration over the summation list input1.txt
+ * and also prints the first duplicate value from iterating over the list multiple times.
  *
- * Run it once to find maximum, approximately double it for list length and use the original value as the offset.
- * For simplicity, if the summation is small enough on the initial run, then the maximum will likely be very close
- * to the absolute value of the largest value in the input list.
+ * Records the maximum sum in the list as a pivot value with some additional tolerance TOLR.
  */
-int main(int argc, char *argv[])
+int main()
 {
-    int sum = 0, n = 0, first = 0;
-    char freq[MAX_LINE];
-    char *list = calloc(160000, sizeof(list));
-    if (!list) {printf("No memory\n"); return 1;}
+    int i, sum, n, max, freqs[IN_LEN] = {0};
+    char curline[MAX_LINE], *sumlist;
 
-    if (argc > 1)
+    sum = n = max = 0;
+    while (fgets(curline, sizeof(curline), stdin))
     {
-        FILE *file = fopen(argv[1], "r");
-
-        if (!file)
-        {
-            printf("Can't open file: %s\n", argv[1]);
-            return -1;
-        }
-
-        while (n < NUM_LOOP)
-        {
-            while (fgets(freq, MAX_LINE, file))
-            {
-                sum += atoi(freq);
-                if (list[sum + 80000] == 1)
-                {
-                    printf("duplicate: %d\n", sum);
-                    fclose(file);
-                    return 1;
-                }
-                list[sum + 80000] = 1;
-            }
-            rewind(file);
-            n++;
-
-            if (!first) {printf("First sum: %d\n", sum); first = 1;}
-        }
-
-        printf("total freq: %d\n", sum);
-        fclose(file);
+        freqs[n] = atoi(curline);
+        if (abs(freqs[n]) > max) {max = abs(freqs[n]);}
+        sum += freqs[n++];
     }
-    else {printf("input filename as first argv\n");}
+    printf("Sum of first iteration: %d\n", sum);
 
-    free(list);
+    max += TOLR;
+    sumlist = calloc(2 * max, sizeof(sumlist));
+    if (!sumlist) {printf("Not enough memory\n"); return 1;}
 
+    sum = 0;
+    while (1)
+    {
+        for (i = 0; i < n; i++)
+        {
+            sum += freqs[i];
+            if (sumlist[sum + max])
+            {
+                printf("Duplicate: %d\n", sum);
+                goto end;
+            }
+            sumlist[sum + max] = 1;
+        }
+    }
+
+end:free(sumlist);
     return 0;
 }
